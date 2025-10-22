8bf3a01f56e134d09ebcd1af418e341863bdd031
ebakernz <emma@psdigital.co.nz>
2025-10-23 11:07:59 +1300

    fix colour

--- src/Extensions/DashboardAdminExtension.php
+++ src/Extensions/DashboardAdminExtension.php
@@ -10,16 +10,5 @@ class DashboardAdminExtension extends Extension
     public function updateInit()
     {
         Requirements::css('plasticstudio/dashboard:client/css/dashboard-panels.css');
-
-        $config = $this->owner->config();
-
-        if ($panelAccentColor = $config->get('panel_accent_color')) {
-            Requirements::customCSS(<<<CSS
-.cms-content.DashboardAdmin .dashboard-panel {
-    border-top-color: $panelAccentColor;
-}
-CSS
-            );
-        }
     }
 }
